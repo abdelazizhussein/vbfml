f38a33132cd1029cc52d92677ed534b3d47eab90
Abdelaziz Hussein <ahussein@bu.edu>
2022-04-20 15:22:25 -0400

    input file modified

--- vbfml/training/input.py
+++ vbfml/training/input.py
@@ -7,19 +7,22 @@ from vbfml.training.util import get_n_events
 
 
 def build_sequence(
-    datasets: "list[DatasetInfo]", features: "list[str]", absolute_weight=False
+    datasets: "list[DatasetInfo]",
+    features: "list[str]",
+    weight_expression: str = "weight_total*xs/sumw",
+    absolute_weight: bool = False,
 ) -> MultiDatasetSequence:
     """Shortcut to set up a MultiDatasetSequence"""
 
-    weight_expression = "weight_total * xs / sumw"
     if absolute_weight:
         weight_expression = f"abs({weight_expression})"
+
     sequence = MultiDatasetSequence(
         batch_size=50,
         branches=features,
         shuffle=True,
         batch_buffer_size=int(1e5),
-        weight_expression="weight_total*xs/sumw",
+        weight_expression=weight_expression,
     )
 
     for dataset in datasets:
@@ -33,7 +36,8 @@ def dataset_from_file_name_bucoffea(filepath: str) -> str:
     Decodes data set name from file name, assuming bucoffea origin.
     """
     base = os.path.basename(filepath)
-    m = re.match("tree_(.*_\d{4}).root", base)
+    m = re.match("tree_(.*).root", base)
+    print(m.groups())
     if not m:
         raise ValueError(f"Could not parse input file name: {filepath}")
     dataset_name = m.groups()[0]
@@ -43,13 +47,13 @@ def dataset_from_file_name_bucoffea(filepath: str) -> str:
 def load_datasets_bucoffea(directory: str) -> "list[DatasetInfo]":
     """
     Load data sets based on a directory of ROOT files from bucoffea.
-
     Data set names are decoded from the file names, assuming one file per data set.
     By default, the labels and names of the data sets are set equal.
     """
     files = glob.glob(f"{directory}/*root")
     datasets = []
     for file in files:
+        print(file)
         dataset_name = dataset_from_file_name_bucoffea(file)
         if "2018" in dataset_name:
             continue
